bdced6171405f9b391bdedccacb5d66d833d60c6
Heath Jones <hj1980@gmail.com>
2016-06-18 18:41:59 +1000

    added intial Logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Logo extends Component {
+  render() {
+    return (
+			<div className="center">
+				<img src="/media/logo.png"/>
+				<h3>HotPotatoe</h3>
+			</div>
+    )
+  }
+}
+
+export default Logo;
